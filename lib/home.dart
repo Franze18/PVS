2184839debea73fc1543a1f964e5b332d3df6a24
Franze Ross Arroyo <arroyofranzeross@gmail.com>
2024-06-25 12:03:49 +0800

    nilagyan prof pic at button at icons

---
+++ lib/home.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Text('User Profile',
+          style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    radius: 50.0,
+                    backgroundColor: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/mambabalagbag.jpg'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.person,
+                      color: Colors.white70,
+                    ),
+                    SizedBox(width: 3.0,),
+                    Text(
+                      'Name:',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black87,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Franze Ross Arroyo',
+                  style: TextStyle(
+                    fontSize:25.0 ,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Row(
+                  children: [
+                    Icon(Icons.calendar_today,
+                      color: Colors.white70,
+                    ),
+                    SizedBox(width: 3.0,),
+                    Text(
+                      'Year:',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black87,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Third Year',
+                  style: TextStyle(
+                    fontSize:25.0 ,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Row(
+                  children: [
+                    Icon(Icons.email,
+                      color: Colors.white70,
+                    ),
+                    SizedBox(width: 3.0,),
+                    Text(
+                      'Email:',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black87,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'arroyofranzeross@gmail.com',
+                  style: TextStyle(
+                    fontSize:25.0 ,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                    onPressed: (){},
+                    child: Text('Add Year'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/home.dart';
 
 void main() => runApp(MaterialApp(
-  home:Scaffold(
-    backgroundColor: Colors.lightBlue,
-    appBar: AppBar(
-      title: Text('User Profile',
-          style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
-    ),
-    backgroundColor: Colors.blue,
-    centerTitle: true,
-  ),
-  body: Padding(
-    padding: EdgeInsets.all(20.0),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          'NAME:',
-          style: TextStyle(
-            fontSize: 16.0,
-            color: Colors.black87,
-            letterSpacing: 2.0,
-          ),
-        ),
-        Text(
-          'Franze Ross Arroyo',
-          style: TextStyle(
-            fontSize:25.0 ,
-            fontWeight: FontWeight.bold,
-            color: Colors.black87,
-          ),
-        ),
-        SizedBox(height: 30.0),
-        Text(
-          'YEAR:',
-          style: TextStyle(
-            fontSize: 16.0,
-            color: Colors.black87,
-            letterSpacing: 2.0,
-          ),
-        ),
-        Text(
-          'Third Year',
-          style: TextStyle(
-            fontSize:25.0 ,
-            fontWeight: FontWeight.bold,
-            color: Colors.black87,
-          ),
-        ),
-        SizedBox(height: 30.0),
-        Text(
-          'Email:',
-          style: TextStyle(
-            fontSize: 16.0,
-            color: Colors.black87,
-            letterSpacing: 2.0,
-          ),
-        ),
-        Text(
-          'arroyofranzeross@gmail.com',
-          style: TextStyle(
-            fontSize:25.0 ,
-            fontWeight: FontWeight.bold,
-            color: Colors.black87,
-          ),
-        ),
-      ],
-    ),
-  ),
-),
+  home: Home(),
 ));
+
 
 
