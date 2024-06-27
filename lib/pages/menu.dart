12536971ee35411e3e80a54020f3ea6f52edcccb
Franze Ross Arroyo <arroyofranzeross@gmail.com>
2024-06-27 11:54:40 +0800

    added products to menu

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/services/product.dart';
+import 'package:untitled/services/menuCard.dart';
 
 class Menu extends StatefulWidget {
   const Menu({super.key});
@@ -8,6 +10,14 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
+  List products = <Product>[
+    Product(productName: "Puge T-shirt", price: 1000.99),
+    Product(productName: "Puge T-shirt Classic Style", price: 1499.99),
+    Product(productName: "Puge T-shirt Retro Style", price: 1999.99),
+    Product(productName: "Puge T-shirt Girl Style", price: 1399.99),
+    Product(productName: "Puge T-shirt Eme Style", price: 1299.99),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,24 +27,13 @@ class _MenuState extends State<Menu> {
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.teal,
+        foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: Container(
+      body: Padding(
+        padding: EdgeInsets.all(3.0),
         child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: (){
-                Navigator.pushNamed(context,'/');
-              },
-              child: Text('Go to Dashboard'),
-            ),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.pushNamed(context,'/profile');
-              },
-              child: Text('Go to Profile'),
-            ),
-          ],
+          children: products.map((product) => Menucard(product: product)).toList(),
         ),
       ),
     );
